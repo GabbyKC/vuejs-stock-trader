b08f599b44f3c664dc0fa3270b153ff58ee304ab
Gabriella Cannon <gabriellakate04@gmail.com>
2020-02-14 17:10:14 +0100

    create header & add routing

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
-  <div id="app" class="container"></div>
+  <div id="app" class="container">
+    <appHeader></appHeader>
+    <div>
+      <div>
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  components: {
+    appHeader: Header
+  }
+};
+</script>
 
 <style>
 #app {
@@ -9,5 +25,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+body {
+  padding: 20px;
 }
 </style>
