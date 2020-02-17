f5e5f54bfdaea4ed4b9a25e8721c6ed52fa746ef
Gabriella Cannon <gabriellakate04@gmail.com>
2020-02-17 12:41:15 +0100

    stock and stocks component updated

---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="col-sm-6 col-md-4">
+    <div class="card bg-light mb-3">
+      <div class="card-body">
+        <h5 class="card-title text-left">{{ stock.name }}</h5>
+        <h6 class="card-subtitle mb-2 text-muted text-left">
+          (Price: {{ stock.price }})
+        </h6>
+        <div class="float-left">
+          <input
+            type="number"
+            class="form-control"
+            placeholder="Quantity"
+            v-model="quantity"
+          />
+        </div>
+        <div class="float-right">
+          <button
+            class="btn btn-success"
+            @click="buyStock"
+            :disabled="quantity <= 0 || !Number.isInteger(+quantity)"
+          >
+            Buy
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      console.log(order);
+      this.quantity = 0;
+    }
+  }
+};
+</script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,35 @@
 <template>
-  <h1>Stocks</h1>
+  <div class="container">
+    <div class="row">
+      <Stock
+        v-for="(stock, index) in stocks"
+        :key="index"
+        :stock="stock"
+      ></Stock>
+    </div>
+  </div>
 </template>
+
+<script>
+import Stock from "./Stock";
+export default {
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "BMW", price: 110 },
+        { id: 2, name: "Tesla", price: 210 },
+        { id: 3, name: "Google", price: 90 },
+        { id: 4, name: "Apple", price: 240 }
+      ]
+    };
+  },
+  components: { Stock }
+};
+</script>
+
+<style scoped>
+.container {
+  padding: 0;
+  margin-top: 40px;
+}
+</style>
